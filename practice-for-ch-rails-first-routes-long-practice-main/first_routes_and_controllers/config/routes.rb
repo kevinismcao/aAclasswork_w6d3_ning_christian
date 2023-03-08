c5b0a7c7a613905713804776d8789283ca2258e4
ningxiao <dsgrichard@hotmail.com>
2023-03-08 15:14:49 -0800

    firstroutes_phase3

---
+++ practice-for-ch-rails-first-routes-long-practice-main/first_routes_and_controllers/config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  resources :users
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+
+  # get 'users/:id', to: 'users#show', as: 'user'
+
+
+end
